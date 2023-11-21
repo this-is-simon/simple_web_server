df90ba63a06414c155225158ad902c45b9712680
Simon Atkins <simon@vault.fan>
2023-11-21 09:09:30 +0000

    Follow same model-controller-view pattern for updating Messages endpoints

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,20 @@
+const model = require("../models/messages.model");
+
 function getMessages(req, res) {
-  res.send("<ul><li>Hello Simon.</li></ul>");
+  res.json(model);
 }
 
 function postMessage(req, res) {
   console.log("updating messages...");
+  if (!req.body.message) {
+    return res.status(400).json({ error: "Message is empty" });
+  }
+  const newMessage = {
+    id: model.length,
+    name: req.body.message,
+  };
+  model.push(newMessage);
+  res.json(newMessage);
 }
 
 module.exports = { getMessages, postMessage };
---
+++ models/messages.model.js
@@ -0,0 +1,7 @@
+const messages = [
+  { id: 0, message: "You're simply the best" },
+  { id: 1, message: "Better than all the rest" },
+  { id: 2, message: "Better than everyone" },
+];
+
+module.exports = messages;
